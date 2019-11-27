8428da3bd6ad16e218b5da61d613e5760a512232
devonChurch <notifydevon@gmail.com>
2019-11-27 20:46:49 +1300

    Updates

---
+++ precache-manifest.c959ad407eff305b925da092b11540e5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efed9b3f58c17ee34f1f868a79affa32",
+    "url": "/avocado/index.html"
+  },
+  {
+    "revision": "588b1b2e7f48881972dd",
+    "url": "/avocado/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "588b1b2e7f48881972dd",
+    "url": "/avocado/static/js/2.ceae832f.chunk.js"
+  },
+  {
+    "revision": "13b88169f0c612f65f3a",
+    "url": "/avocado/static/js/main.4a9df38c.chunk.js"
+  },
+  {
+    "revision": "3a13e36650f3a46b87c1",
+    "url": "/avocado/static/js/runtime~main.fdc3e02c.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.51195f1163d2abd8bbbe50c2c03e60d1.js"
+  "/avocado/precache-manifest.c959ad407eff305b925da092b11540e5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/avocado/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
